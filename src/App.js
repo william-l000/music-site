66c1139875f30ccef14539ce4aa78d9b1209e453
william_lin <123@163.com>
2021-03-26 19:03:47 +0800

    implement header component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
-import './App.scss';
+import { BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
+import HomePage from '@/pages/HomePage'
+import MyMusicPage from '@/pages/MyMusicPage'
+import FindMusicPage from '@/pages/FindMusicPage'
+import MyFriendPage from '@/pages/MyFriendPage'
+import DownloadPage from '@/pages/DownloadPage'
 
 function App() {
   return (
-    <div className="test">
-    test
-    </div>
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/home" component={HomePage}/>
+          <Route path="/mymusic" component={MyMusicPage}/>
+          <Route path="/findmusic" component={FindMusicPage}/>
+          <Route path="/myfriend" component={MyFriendPage}/>
+          <Route path="/download" component={DownloadPage}/>
+          <Redirect exact path="/" to={"/home"} />
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 }
 
